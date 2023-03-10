547a37f3b49b3c6533b7272112d316172e3e883e
LENOVO <luisochoa.1495@gmail.com>
2023-03-10 16:08:13 -0500

    10/03/2023-2

--- ENVIAR_EMAIL/enviar_email_smtlib.py
+++ ENVIAR_EMAIL/enviar_email_smtlib.py
@@ -1,7 +1,12 @@
-from email.message import EmailMessage
-import smtplib
+"""
+SMTP
+¿Qué es y para qué sirve SMTP?
+SMTP, Simple Mail Transfer Protocol por sus siglas en inglés, es un protocolo o conjunto de reglas 
+de comunicación que utilizan los servidores de correo electrónico para enviar y recibir e-mails.
+"""
+from email.message import EmailMessage #Construir la estructura del email
+import smtplib # conectar con el servidor y enviarlo
 from tkinter import *
-import tkinter as tk
 from tkinter import messagebox
 #Python image Library
 from PIL import ImageTk, Image
@@ -9,15 +14,15 @@ from PIL import ImageTk, Image
 "------------INTERFAZ TKINTER------------"
 ventana =Tk()
 ventana.title("ALICACION DE MENSAJERIA")
-ventana.geometry("338x385")
+ventana.geometry("338x365")
 ventana.resizable(0,0)
 ventana.config(bd=10)
 
-Label(ventana, text="ENVIAR CORREO GMAIL",fg="black",font=("Arial", 15,"bold"),padx=5,pady=5).grid(row=0,column=0,columnspan=2)
+Label(ventana, text="ENVIAR CORREO VIA GMAIL",fg="black",font=("Arial", 15,"bold"),padx=5,pady=5).grid(row=0,column=0,columnspan=2)
 
-#Imagen calculadora
-imagen_calculadora=Image.open("D:/EIGHTA/PYTHON-TKINTER/ENVIAR_EMAIL/logo_gmail.png")
-nueva_imagen=imagen_calculadora.resize((120,82))
+#Imagen GMAIL
+imagen_gmail=Image.open("D:/EIGHTA/PYTHON-TKINTER/ENVIAR_EMAIL/logo_gmail.png")
+nueva_imagen=imagen_gmail.resize((125,84))
 render=ImageTk.PhotoImage(nueva_imagen)
 label_imagen= Label(ventana, image= render)
 label_imagen.image=render
@@ -26,9 +31,8 @@ label_imagen.grid(row=1,column=0,columnspan=2)
 #Variables
 destinatario=StringVar(ventana)
 asunto=StringVar(ventana)
-mensaje2=StringVar(ventana)
 
-Label(ventana, text="Mi correo: luisochoa.1495@gmail.com",fg="white",bg="blue",font=("Arial", 10,"bold"),padx=5,pady=5).grid(row=2,column=0,columnspan=2,pady=5)
+#Label(ventana, text="Mi correo: luisochoa.1495@gmail.com",fg="white",bg="blue",font=("Arial", 10,"bold"),padx=5,pady=5).grid(row=2,column=0,columnspan=2,pady=5)
 
 Label(ventana, text="Destinatario:",fg="black",font=("Arial", 10,"bold"),padx=5,pady=5).grid(row=3,column=0)
 Entry(ventana,textvariable=destinatario, width=34).grid(row=3,column=1)
@@ -45,15 +49,15 @@ mensaje.config(font=("Arial", 9),padx=5, pady=5)
 "------------ENVIO DE CORREO------------"
 def enviar_email():
     remitente = "luisochoa.1495@gmail.com"
-    #destinatario = "danite.dev@gmail.com"
-    #mensaje = "¡Hola, mundo!"
+    #Estrutura de email
     email = EmailMessage()
     email["From"] = remitente
     email["To"] = destinatario.get()
     email["Subject"] = asunto.get()
     email.set_content(str(mensaje.get(1.0, 'end')))
+    #Envio de email
     smtp = smtplib.SMTP_SSL("smtp.gmail.com")
-    smtp.login(remitente, "ppowftoxeatqdcov")
+    smtp.login(remitente, "suttsabovppzvsep")
     smtp.sendmail(remitente, destinatario.get(), email.as_string())
     messagebox.showinfo("MENSAJERIA","Mensaje enviado correctamente ")
     smtp.quit()
